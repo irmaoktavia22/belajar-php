e652f4b7975c30ac800a89621c66fb3695888ca1
Irma Oktavia <oktaviairma22@gmail.com>
2022-10-24 20:46:30 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+echo "Hello World!<br>";
+
+$nama = "Irma Oktavia";
+$umur = 21;
+
+echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun.<br>";
+
+$namaKakak = "Ria Meytalia";
+$umurKakak = 26;
+
+echo "Nama kakak saya <strong>$namaKakak</strong>, kakak saya berusia 
+$umurKakak tahun.<br>";
+
+$selisihUmur = $umur - $umurKakak;
+
+echo "Selisih usia saya dan kakak saya adalah $selisihUmur tahun.";
+
+?>
+
+
+
+
+
